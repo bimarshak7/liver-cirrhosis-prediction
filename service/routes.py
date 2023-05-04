00c17ad85aba12d3e3caace2b9eba72256b4bbc8
bimarshak7 <khanalbimarsha7@gmail.com>
2023-05-04 17:42:04 +0545

    Predict with multiple model

--- service/routes.py
+++ service/routes.py
@@ -7,7 +7,12 @@ bp = Blueprint('predict_routes', __name__)
 
 # Load the saved model
 scaler = joblib.load('../models/scaler.joblib')
-model = joblib.load('../models/ada_best.joblib')
+ada_best = joblib.load('../models/ada_best.joblib')
+ada_full = joblib.load('../models/ada_full.joblib')
+bag_best = joblib.load('../models/bag_fs.joblib')
+rf = joblib.load('../models/rf_full.joblib')
+svm = joblib.load('../models/svm.joblib')
+
 best_cols = np.array([ 0,  2,  4,  5,  6,  8,  9, 10, 11, 13, 15, 16])
 
 @bp.route('/predict')
@@ -19,11 +24,21 @@ def index():
 def predict():
     features = process(request.form).reshape(1,-1)
     features = scaler.transform(features)
-    print(features)
-    prediction = model.predict(features[:,best_cols])
-    print("prediction ",prediction)
-    
-    return {"prediction":int(prediction[0])}
+    model = request.form['Model']
+    if model=='ada-best':
+        prediction = ada_best.predict(features[:,best_cols])
+    elif model=='ada_full':
+        prediction = ada_full.predict(features)
+    elif model=='bag_best':
+        prediction = bag_best.predict(features[:,best_cols])
+    elif model=='rf':
+        prediction = rf.predict(features)
+    else:
+        prediction = svm.predict(features[:,best_cols])
+    print("Model: ",model," ,prediction: ",prediction[0])
+
+    message = "Tha patient might have stage 4 cirrhosis.It's critical!" if prediction[0]==4 else "The patient might have stage 3 cirrhosis. Immediate treatment required!" if prediction[0]==3 else "The patient might have stage 2 cirrhosis. Recommend him/her a healthy routine!"
+    return {"prediction":int(prediction[0]),"message":message}
 
 def process(form):
     n_days = int(request.form['N_Days'])
@@ -46,6 +61,7 @@ def process(form):
 
     features = np.array([n_days,drug,age,sex,ascites,hepatomegaly,spiders,edema,bilirubin,cholesterol,albumin,copper,alk_phos,sgot,tryglicerides,platelets,prothrombin])
 
+    features[8:] = np.log(features[8:])
     
 
     return features
